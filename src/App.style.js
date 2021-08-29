3c857b43c795a158fcb220e5eb4552b806feabf1
Manuel Girón <manuelgan@ext.stradivarius.com>
2021-08-29 12:50:59 +0200

    - changed the way to import styled-components to be able to debug in an easy way from the dev-tools.

--- src/App.style.js
+++ src/App.style.js
@@ -1,4 +1,4 @@
-import styled, { createGlobalStyle, css } from "styled-components";
+import styled, { createGlobalStyle, css } from "styled-components/macro";
 
 export const GlobalStyles = createGlobalStyle`
   * {
--- src/components/CartRow/CartRow.style.js
+++ src/components/CartRow/CartRow.style.js
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled from "styled-components/macro";
 import { VerticalCenter } from "../../App.style";
 
 export const CartRowContainer = styled.div`
