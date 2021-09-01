4789efbe526fd24a1e8c2dbb1f63ee1ee8d0f2e2
Manuel Girón <manuelgan@ext.stradivarius.com>
2021-09-01 12:45:39 +0200

    - The Loader logic has been refactored to a new component, to keep the App.js as clean as posible.

---
+++ src/components/Loader/Loader.style.js
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+export const LoaderContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  background-color: rgba(0, 0, 0, 0.8);
+  z-index: 10;
+`;
+
+export const StyledLoader = styled.div`
+  position: absolute;
+  margin: auto;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  width: 150px;
+  height: 150px;
+`;
